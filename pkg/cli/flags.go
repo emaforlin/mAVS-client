754b10bd6293d8cd3b7d68cb1d5948ceabe0e41a
Emanuel <emaa.forlin@gmail.com>
2024-01-11 09:57:41 -0300

    fix a typo

--- pkg/cli/flags.go
+++ pkg/cli/flags.go
@@ -51,7 +51,7 @@ var config Config
 
 func ParseFlags() (Config, error) {
 	flag.UintVar(&config.MiningDifficulty, "m", 5, "Sets the mining difficulty")
-	flag.StringVar(&config.RendezvousString, "rendesvouz", "meet me at Agriculture Park",
+	flag.StringVar(&config.RendezvousString, "rendezvous", "meet me at Agriculture Park",
 		"Unique string used to identify a group of nodes. Share this with other nodes so they can connect to yours")
 	flag.Var(&config.BootstrapPeers, "peer", "Adds a peer multiaddress to the bootstrap list")
 	flag.Var(&config.ListenAddresses, "listen", "Adds a multiaddress to the listen list")
